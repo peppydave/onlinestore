f46470b4ac08ade8f4862aa659df9c79187c66d9
Pelumi <okusanyap@gmail.com>
2017-03-28 16:01:46 +0100

    changing the way

--- www/test.php
+++ www/test.php
@@ -2,6 +2,15 @@
 define ('DBNAME', 'onlinestore');
 define('DBUSER', 'root');
 define('DBPASS', 'peppydev');
+
+try{
+	# prepare a pdo instance
 $pdo = new PDO ('mysql:host=localhost;dbname =' .DBNAME, DBUSER, DBPASS);
+
+	# set verbose error modes
+	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_SILENT);
+} catch(PDOException $e){
+	echo $e->getmessage();
+}
 
 ?>
