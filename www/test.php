75cb6e7784e7e6d52db26b23f80700c94b2ddc2c
Pelumi <okusanyap@gmail.com>
2017-03-28 17:11:49 +0100

    putting maximum file size

--- www/test.php
+++ www/test.php
@@ -13,6 +13,33 @@ $pdo = new PDO ('mysql:host=localhost;dbname =' .DBNAME, DBUSER, DBPASS);
 	echo $e->getmessage();
 }*/
 
+
+#max file size
+
+
+define("MAX_FILE_SIZE", "2097152");
+
+$ext = ["image/jpg", "image/jpeg", "image/png"]
+	if(array_key_exists('save', $_POST)) {
+		$errors = [];
+
+	#Be sure a file was selcted
+	if (empty($_FILES['pic']['name'])) {
+		$errors[] = "Please choose a file";
+	}
+	#check file size
+	if ($_FILES['pic']['size'] > MAX_FILE_SIZE) {
+		$errors[] = "file size exceeds maximum. maximum: ".MAX_FILE_SIZE;
+	}
+	if(empty($errors)) {
+		echo "done";
+	} else {
+		foreach ($errors as $err) {
+			echo $err. '</br>';
+		}
+	}
+}
+
 ?>
 
 <form id="register" method="POST" enctype="multipart/form-data"> 
