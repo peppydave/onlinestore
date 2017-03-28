8edd16914c77ca7c2dd8fb5daa23c60eac39e9b4
Pelumi <okusanyap@gmail.com>
2017-03-28 16:07:06 +0100

    first try in file uploads

--- www/test.php
+++ www/test.php
@@ -1,5 +1,5 @@
 <?php
-define ('DBNAME', 'onlinestore');
+/*define ('DBNAME', 'onlinestore');
 define('DBUSER', 'root');
 define('DBPASS', 'peppydev');
 
@@ -11,6 +11,14 @@ $pdo = new PDO ('mysql:host=localhost;dbname =' .DBNAME, DBUSER, DBPASS);
 	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_SILENT);
 } catch(PDOException $e){
 	echo $e->getmessage();
-}
+}*/
 
 ?>
+
+<form id="register" method="POST" enctype="multipart/form-data"> 
+<p>Please upload a file</p>
+<input type="file" name="pic">
+<input type="submit" name="save">
+
+
+</form>
