0b52574a2a268e55ff14f53d1158418f3a15639e
Pelumi <okusanyap@gmail.com>
2017-03-29 18:54:05 +0100

    validating other form fields

--- www/register.php
+++ www/register.php
@@ -29,7 +29,7 @@ if(array_key_exists('register', $_POST)) {
 	}
 
 	if($_POST['pword'] != $_POST['password']) {
-		$errors[pword] = "passwords do not match";
+		$errors['pword'] = "passwords do not match";
 	}
 	
 	}
@@ -58,20 +58,36 @@ if(array_key_exists('register', $_POST)) {
 				<input type="text" name="fname" placeholder="first name">
 			</div>
 			<div>
+				<?php
+			if(isset($errors['lname'])) 
+				{echo '<span class="err">'. $errors['lname']. '</span>';}
+			?>
 				<label>last name:</label>	
 				<input type="text" name="lname" placeholder="last name">
 			</div>
 
 			<div>
+			<?php
+			if(isset($errors['email'])) 
+				{echo '<span class="err">'. $errors['email']. '</span>';}
+			?>
 				<label>email:</label>
 				<input type="text" name="email" placeholder="email">
 			</div>
 			<div>
+			<?php
+			if(isset($errors['password'])) 
+				{echo '<span class="err">'. $errors['password']. '</span>';}
+			?>
 				<label>password:</label>
 				<input type="password" name="password" placeholder="password">
 			</div>
  
 			<div>
+			<?php
+			if(isset($errors['pword'])) 
+				{echo '<span class="err">'. $errors['pword']. '</span>';}
+			?>
 				<label>confirm password:</label>	
 				<input type="password" name="pword" placeholder="password">
 			</div>
